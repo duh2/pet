eb8bb315e74b7b84a5a69e97782f3091f5e6fd19
duh2 <hristabudda2@gmail.com>
2021-01-20 02:01:38 +0700

    exported js and css to separate files

---
+++ HideBoxes.js
@@ -0,0 +1,28 @@
+function hideboxes() {
+    let malebox = document.getElementsByClassName('box-male');
+    let checkmale = document.querySelector("#checkMale");
+    let femalebox = document.getElementsByClassName('box-female');
+    let checkfemale = document.querySelector("#checkfemale")
+    if (checkmale.checked){
+        for(let i=0; i<malebox.length;i++){
+            malebox[i].classList.remove(`hidden`) ;
+        }
+    } else{
+        for(let i=0; i<malebox.length;i++){
+            malebox[i].classList.add(`hidden`) ;
+        }
+    }
+    if (checkfemale.checked){
+        for(let i=0; i<femalebox.length;i++){
+            femalebox[i].classList.remove(`hidden`) ;
+        }
+    } else{
+        for(let i=0; i<femalebox.length;i++){
+            femalebox[i].classList.add(`hidden`) ;
+        }
+    }
+
+
+
+}
+
