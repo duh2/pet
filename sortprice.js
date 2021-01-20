1f967f4188cea34d2267be41a7744568e1d4b5c2
duh2 <hristabudda2@gmail.com>
2021-01-20 17:19:00 +0700

    deleted alert

--- sortprice.js
+++ sortprice.js
@@ -3,13 +3,13 @@ function My_filter() {
     let price_box_value
     let price_box = document.getElementsByClassName('cost');
     let all_boxes = document.getElementsByClassName('box');
-    alert(price_box[2])
+
     for (let i=0; i<price_box.length; i++){
         price_box_value = price_box[i].getAttribute('data-value_price')
 
         if (price_box_value<price){
             all_boxes[i].classList.add('hidden')
-        }
+        } else all_boxes[i].classList.remove('hidden')
 
 
     }
