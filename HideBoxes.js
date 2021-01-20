cbc0198377bef65d4d19202ac3428c8f5572bbbc
duh2 <hristabudda2@gmail.com>
2021-01-20 22:05:41 +0700

    added using of cookies

--- HideBoxes.js
+++ HideBoxes.js
@@ -26,3 +26,4 @@ function hideboxes() {
 
 }
 
+
--- sortprice.js
+++ sortprice.js
@@ -1,8 +1,10 @@
-function My_filter() {
+let price ;
+function my_filter() {
     let price = parseInt(document.querySelector('#pricebox').value);
     let price_box_value
     let price_box = document.getElementsByClassName('cost');
     let all_boxes = document.getElementsByClassName('box');
+    document.cookie='price_cookie=' + price;
 
     for (let i=0; i<price_box.length; i++){
         price_box_value = price_box[i].getAttribute('data-value_price')
@@ -13,4 +15,31 @@ function My_filter() {
 
 
     }
+}
+function getCookie(name) {
+    let cookie = " " + document.cookie;
+    let search = " " + name + "=";
+    let setStr = null;
+    let offset = 0;
+    let end = 0;
+    if (cookie.length > 0) {
+        offset = cookie.indexOf(search);
+        if (offset !== -1) {
+            offset += search.length;
+            end = cookie.indexOf(";", offset);
+            if (end === -1) {
+                end = cookie.length;
+            }
+            setStr = decodeURI(cookie.substring(offset, end));
+        }
+    }
+    return(setStr);
+}
+function setCookie() {
+    let price_cookie=getCookie('price_cookie')
+    alert(price_cookie)
+    if ( price_cookie!==null){
+        document.querySelector('#pricebox').value=price_cookie;
+    }
+
 }
