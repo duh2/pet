6cc69e11a2b867e3c23dd1ccdc0704439f02d788
duh2 <hristabudda2@gmail.com>
2021-03-02 17:08:51 +0700

    trying to make xhr request

--- src/App.js
+++ src/App.js
@@ -1,32 +1,59 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
-import load from "./load";
-import cell from "./cell";
-load('http://localhost:3001/products').then(data=>{
-
-})
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      data: null,
+      data: [],
+      isLoading: false,
     }
-    this.loadData()
   }
-
-  loadData() {
-    load(this.props.data).then(products => {
-      this.setState({data: JSON.parse(products)})
+  getJSONdata(){
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', 'http://localhost:3000/products', true);
+    xhr.send();
+    this.setState({isLoading:true})
+    xhr.onreadystatechange = ()=>{
+      if (xhr.readyState!==4){
+        return false
+      }
+      if (xhr.status!== 200){
+        console.log(xhr.status + ': ' + xhr.statusText)
+      } else {
+        this.setState({
+          data: JSON.parse(xhr.responseText),
+          isLoading:false,
+        })
+      }
+    }
+  }
+renderProducts(){
+    const {data, isLoading} = this.state
+  if (isLoading){
+    return <div>loading....</div>
+  } else {
+    return data.map(item=>{
+     return(<samplebox className='box'>
+      <img src={item.Img} alt={item.Model} className='pic'/>
+          <div data-name={item.Model} className='name'>{item.Model}</div>
+          <div className={item.Sex}>{item.Sex}</div>
+          <div className='cost' data-value-price={item.Price}>{item.Price}</div>
+            </samplebox>)
     })
   }
+}
 
-  updateData(config) {
-  this.setState(config)
-  }
+
   render() {
-    return (<div>Hello, biba</div>)
+    return (
+    <div className='article1'>
+      {this.renderProducts()}
+    </div>
+
+    )
   }
+
 }
   export default App;
